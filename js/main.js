581d8dac52ddef71fcfea8a6bda7b2fa59257f56
Aman Khan <khanamanatwork@gmail.com>
2022-08-25 17:45:55 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+  
+/*
+</script>
+        function openNav(){
+            document.getElementById("side-nav").style.display = "block";
+            document.getElementById("close-nav").style.display = "block";
+        }
+        function closeNav(){
+            document.getElementById("side-nav").style.display = "none";
+            document.getElementById("close-nav").style.display = "none";
+        }
+    </script> */
